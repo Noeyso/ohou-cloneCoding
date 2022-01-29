9264ea0f66061545649eb80a74987017418a97bd
noeyso <soyeon9819@gmail.com>
2022-01-29 18:39:38 +0900

    메인페이지 사진, 배너

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React, { useRef, useState } from "react";
 import styles from "./app.module.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./components/header/header";
+import Home from "./page/community/home/home";
 
 function App() {
   return (
     <div className={styles.container}>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <div className={styles.page}>
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/header/subHeader/subHeader.tsx
+++ src/components/header/subHeader/subHeader.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./subHeader.module.css";
 import Icon from "../../../common/images/icon.png";
 
@@ -6,11 +6,17 @@ interface Props {
   menu: string[];
 }
 function SubHeader({ menu }: Props) {
+  const [isClicked, setIsClicked] = useState(false);
+
+  function clickMenu() {
+    setIsClicked(true);
+  }
+
   return (
     <div className={styles.container}>
       <ul className={styles.menu}>
         {menu.map((menuItem, idx) => (
-          <li className={styles.menu_item} key={idx}>
+          <li className={styles.menu_item} key={idx} onClick={clickMenu}>
             {menuItem}
           </li>
         ))}
