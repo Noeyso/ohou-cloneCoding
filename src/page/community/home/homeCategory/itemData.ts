30d7ac3fd83b6b6da90feac28e4bda1b45c82a36
noeyso <soyeon9819@gmail.com>
2022-02-07 00:53:22 +0900

    오늘의딜 섹션

---
+++ src/page/community/home/homeCategory/itemData.ts
@@ -0,0 +1,66 @@
+export const menuItems = [
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/164333421927911367.png?gif=1&w=180&h=180&c=c",
+    text: "가구",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/162823225115177697.png?gif=1&w=144&h=144&c=c 1.5x,https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/162823225115177697.png?gif=1&w=180&h=180&c=c 2x,https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/162823225115177697.png?gif=1&w=256&h=256&c=c 3x",
+    text: "패브릭",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/162823225665741013.png?gif=1&w=180&h=180&c=c",
+    text: "조명",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/162823226017937426.png?gif=1&w=144&h=144&c=c",
+    text: "가전",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/162823226903946200.png?gif=1&w=144&h=144&c=c",
+    text: "주방용품",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/162823227719846773.png?gif=1&w=144&h=144&c=c",
+    text: "데코/식물",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/162823228178967968.png?gif=1&w=144&h=144&c=c",
+    text: "수납/정리",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/163609843911228804.png?gif=1&w=144&h=144&c=c",
+    text: "생활용품",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/164333398177552131.png?gif=1&w=144&h=144&c=c",
+    text: "서랍/수납장",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/163575474448469686.png?gif=1&w=144&h=144&c=c",
+    text: "생필품",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/162823230480918647.png?gif=1&w=144&h=144&c=c",
+    text: "공구/DIY",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/162823230908544036.png?gif=1&w=144&h=144&c=c",
+    text: "인테리어시공",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/162823231401891024.png?gif=1&w=144&h=144&c=c",
+    text: "반려동물",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/162823242317048198.png?gif=1&w=144&h=144&c=c",
+    text: "캠핑용품",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/162823231949249287.png?gif=1&w=144&h=144&c=c",
+    text: "실내운동",
+  },
+  {
+    img: "https://image.ohou.se/i/bucketplace-v2-development/uploads/category/store_home_categories/164148743817769527.png?gif=1&w=144&h=144&c=c",
+    text: "렌탈",
+  },
+];
--- src/page/community/home/todayDeal/todayDeal.tsx
+++ src/page/community/home/todayDeal/todayDeal.tsx
@@ -1,28 +1,91 @@
-import React from "react";
+import React, { CSSProperties, useState } from "react";
 import { listItems } from "./itemData";
 import styles from "./todayDeal.module.css";
+import { AiFillStar } from "react-icons/ai";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 function TodayDeal() {
+  const [temp, setTemp] = useState(0);
+  const [remain, setRemain] = useState(1752);
+  const [style, setStyle] = useState<CSSProperties>({});
+
+  function moveNext() {
+    console.log(`temp: ${temp} remain:${remain}`);
+    if (remain === 0) {
+      console.log("뒤로 이동 불가");
+      return;
+    }
+    console.log("move next");
+    if (remain >= 1168) {
+      setRemain(remain - 1168);
+      setTemp(temp + 1168);
+      setStyle({ marginLeft: `${-(temp + 1168)}px` });
+    } else {
+      setTemp(temp + remain);
+      setRemain(0);
+      setStyle({ marginLeft: `${-(temp + remain)}px` });
+    }
+  }
+  function movePrev() {
+    console.log(`temp: ${temp} remain:${remain}`);
+    if (temp === 0) {
+      console.log("앞으로 이동 불가");
+      return;
+    }
+    console.log("move prev");
+    if (temp >= 1168) {
+      setTemp(temp - 1168);
+      setRemain(temp + 1168);
+      setStyle({ marginLeft: `${-(temp - 1168)}px` });
+    } else {
+      setTemp(0);
+      setRemain(remain + temp);
+      setStyle({ marginLeft: `${0}px` });
+    }
+  }
   return (
     <section className={styles.container}>
       <div className={styles.deal}>
-        <ul>
-          {listItems.map((item, idx) => (
-            <li key={idx}>
-              <img src={item.img} alt="item" />
-              <span>{item.company}</span>
-              <span>{item.title}</span>
-              <div>
-                <span>{item.sale}%</span>
-                <span>{item.price}</span>
-              </div>
-              <div>
-                <span>{item.rate}</span>
-                <span>리뷰 {item.review}</span>
-              </div>
-              {item.isFreeShip && <div>무료배송</div>}
-            </li>
-          ))}
-        </ul>
+        <div className={styles.header}>
+          <h2>오늘의딜</h2>
+          <span>더보기</span>
+        </div>
+        <div className={styles.carousel}>
+          <div className={styles.list_prev}>
+            <button className={`${styles.move_btn} ${styles.left}`}>
+              <IoIosArrowBack color="#fff" size="1rem" onClick={movePrev} />
+            </button>
+          </div>
+          <ul className={styles.menu} style={style}>
+            {listItems.map((item, idx) => (
+              <li key={idx} className={styles.item}>
+                <div className={styles.img_box}>
+                  <img src={item.img} alt="item" />
+                </div>
+                <span className={styles.company}>{item.company}</span>
+                <span className={styles.title}>{item.title}</span>
+                <div className={styles.item_price}>
+                  <span className={styles.sale}>{item.sale}%</span>
+                  <span className={styles.price}>{item.price}</span>
+                </div>
+                <div className={styles.item_status}>
+                  <AiFillStar width="1.1rem" height="1.1rem" color="#35c5f0" />
+                  <span className={styles.rate}>{item.rate}</span>
+                  <span className={styles.review}>리뷰 {item.review}</span>
+                </div>
+                {item.isFreeShip && (
+                  <span className={styles.freeShip}>무료배송</span>
+                )}
+              </li>
+            ))}
+          </ul>
+
+          <div className={styles.list_next}>
+            <button className={`${styles.move_btn} ${styles.right}`}>
+              <IoIosArrowForward color="#fff" size="1rem" onClick={moveNext} />
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   );
