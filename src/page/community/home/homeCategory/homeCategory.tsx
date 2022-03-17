6894c0c069c5f862aac93634340810ea61cb0f49
noeyso <soyeon9819@gmail.com>
2022-03-17 16:25:02 +0900

    캐러셀 반응형으로 만들기

--- src/page/community/home/homeCategory/homeCategory.tsx
+++ src/page/community/home/homeCategory/homeCategory.tsx
@@ -1,13 +1,33 @@
-import React, { CSSProperties, useState } from "react";
+import React, {
+  CSSProperties,
+  useState,
+  useEffect,
+  useRef,
+  createRef,
+} from "react";
 import styles from "./homeCategory.module.css";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 import { menuItems } from "./itemData";
 
 function HomeCategory() {
+  const totalItem = menuItems.length;
+  const showItem = 10;
+  const slideItem = 5;
+  const [width, setWidth] = useState(112);
+  // const [temp, setTemp] = useState(0);
+  // const [remain, setRemain] = useState(width * (totalItem - showItem));
   const [temp, setTemp] = useState(0);
-  const [remain, setRemain] = useState(672);
+  const [remain, setRemain] = useState(totalItem - showItem);
+  const [interval, setInterval] = useState(width * slideItem);
   const [style, setStyle] = useState<CSSProperties>({});
+  const [margin, setMargin] = useState(0);
+  const [pullScreen, setPullScreen] = useState(0);
+  const divRef = createRef<HTMLDivElement>();
+  console.log(`carousel width : ${divRef.current?.clientWidth}`);
+  useEffect(() => {
+    console.log(`carousel width : ${divRef.current?.clientWidth}`);
+  }, [window.innerWidth]);
 
   function moveNext() {
     console.log(`temp: ${temp} remain:${remain}`);
@@ -16,14 +36,16 @@ function HomeCategory() {
       return;
     }
     console.log("move next");
-    if (remain >= 560) {
-      setRemain(remain - 560);
-      setTemp(temp + 560);
-      setStyle({ marginLeft: `${-(temp + 560)}px` });
+    if (remain >= slideItem) {
+      setRemain(remain - slideItem);
+      setTemp(temp + slideItem);
+      //setStyle({ marginLeft: `${-(temp + interval)}px` });
+      setMargin(temp + slideItem);
     } else {
       setTemp(temp + remain);
       setRemain(0);
-      setStyle({ marginLeft: `${-(temp + remain)}px` });
+      //setStyle({ marginLeft: `${-(temp + remain)}px` });
+      setMargin(temp + remain);
     }
   }
   function movePrev() {
@@ -33,20 +55,22 @@ function HomeCategory() {
       return;
     }
     console.log("move prev");
-    if (temp >= 560) {
-      setTemp(temp - 560);
-      setRemain(remain + 560);
-      setStyle({ marginLeft: `${-(temp - 560)}px` });
+    if (temp >= slideItem) {
+      setTemp(temp - slideItem);
+      setRemain(remain + slideItem);
+      //setStyle({ marginLeft: `${-(temp - slideItem)}px` });
+      setMargin(temp - slideItem);
     } else {
       setTemp(0);
       setRemain(remain + temp);
-      setStyle({ marginLeft: `${0}px` });
+      //setStyle({ marginLeft: `${0}px` });
+      setMargin(0);
     }
   }
   return (
     <section className={styles.container}>
       <h2>카테고리별 상품 찾기</h2>
-      <div className={styles.carousel}>
+      <div ref={divRef} className={styles.carousel}>
         <div className={styles.list_prev}>
           <button
             className={temp === 0 ? "" : `${styles.move_btn} ${styles.left}`}
@@ -55,9 +79,15 @@ function HomeCategory() {
           </button>
         </div>
 
-        <ul className={styles.list} style={style}>
+        <ul
+          className={styles.list}
+          style={{
+            width: `calc(100% + 100% * ${(totalItem - showItem) / showItem})`,
+            marginLeft: `calc( 100% / ${showItem} * ${-margin})`,
+          }}
+        >
           {menuItems.map((item, idx) => (
-            <li key={idx}>
+            <li key={idx} style={{ width: `calc( 100% / ${showItem})` }}>
               <img src={item.img} alt="icon" />
               {item.text}
             </li>
@@ -74,4 +104,5 @@ function HomeCategory() {
     </section>
   );
 }
+
 export default HomeCategory;
